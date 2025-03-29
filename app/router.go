554d3b709cb532a33e51e5300b4197baee96bbb0
Sleepful <josepablov@gmail.com>
2025-03-29 07:18:24 -0600

    Properly intercept auth endpoint + serve index.js

--- app/router.go
+++ app/router.go
@@ -1,12 +1,17 @@
 package app
 
 import (
+	"fmt"
 	"mymail/app/pages"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
+
+	// "github.com/pocketbase/pocketbase/tools/hook"
 	"github.com/urfave/negroni"
 )
 
@@ -25,25 +30,49 @@ func rootMiddleware(e *core.RequestEvent) (err error) {
 	lrw := negroni.NewResponseWriter(e.Response)
 	e.Response = lrw
 	err = e.Next()
+	if err != nil && strings.Contains(fmt.Sprintf("%s", e.Request.URL), "auth-with-password") {
+		fmt.Printf("<-- %s", err)
+		// return html error message here 'wrong credentials'
+	}
+	if err != nil {
+		// return html error message here 'something went wrong, go back'
+	}
+
 	// Useful to check the Response status after the route resolves:
 	// fmt.Printf("<-- %d %s", lrw.Status(), http.StatusText(lrw.Status()))
 	return err
 }
 
 func MakeRouter(app *pocketbase.PocketBase) {
-	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
+	app.OnRecordAuthRequest().BindFunc(func(e *core.RecordAuthRequestEvent) error {
+		// redirect successful logins
+		return e.Redirect(301, "/site")
+	})
 
-		pageGroup := se.Router.Group("/page")
+	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
 
 		se.Router.BindFunc(rootMiddleware)
 
-		pageGroup.BindFunc(requireAuth) // require auth for this route group
+		// serve static assets
+		se.Router.GET("/assets/{path...}", apis.Static(os.DirFS("assets"), false)).BindFunc(
+			func(e *core.RequestEvent) error {
+				if strings.Contains(fmt.Sprintf("%s", e.Request.URL), "index.js") {
+					e.Response.Header().Set("Content-type", "text/javascript")
+				}
+				return e.Next()
+			})
+
+		pageGroup := se.Router.Group("/page")
+		pageGroup.BindFunc(requireAuth) // require auth for /page* routes
 
 		se.Router.GET("/login", apis.WrapStdHandler(
 			http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				pages.Layout(pages.Login()).Render(r.Context(), w)
 			})))
 
+		/// ends OnServe
+		//
+		//
 		return se.Next()
 	})
 }
